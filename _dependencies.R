3936938e6df21910e8a40b375d13a751bd078dcb
WevertonGomesCosta <weverton.costa@ufv.br>
2026-09-09 16:37:50 -0300

    Convert data design module to tutorial format

--- _dependencies.R
+++ _dependencies.R
@@ -1,3 +1,10 @@
 # Dependências explícitas não detectadas automaticamente pelo renv.
 # caret é utilizado no cálculo das importâncias dos métodos de ML.
 library(caret)
+
+# Dependências visuais do tutorial.
+# ggthemes fornece o tema e as escalas GDocs usadas nas figuras.
+library(ggthemes)
+
+# kableExtra padroniza a apresentação das tabelas no site.
+library(kableExtra)
