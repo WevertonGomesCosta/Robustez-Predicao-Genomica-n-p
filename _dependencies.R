5e3d78b0afe5cfe543edfe1f3abdc66aa6b353fd
WevertonGomesCosta <weverton.costa@ufv.br>
2026-09-10 09:45:34 -0300

    Convert GBLUP module to tutorial format

--- _dependencies.R
+++ _dependencies.R
@@ -2,6 +2,11 @@
 # caret é utilizado no cálculo das importâncias dos métodos de ML.
 library(caret)
 
+# Dependência científica do GBLUP.
+# Os ajustes pesados do tutorial permanecem em chunks com eval = FALSE,
+# por isso sommer precisa ser declarado explicitamente para o renv.
+library(sommer)
+
 # Dependências visuais do tutorial.
 # ggthemes fornece o tema e as escalas GDocs usadas nas figuras.
 library(ggthemes)
